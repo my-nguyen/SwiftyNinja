65eaa035f74e651aa1e8d6e26ad0e4fa3fb7245c
My Nguyen <My@mys-macbook-pro.local>
2016-08-14 14:42:53 -0700

    Basics quick start: SKShapeNode

--- SwiftyNinja/GameScene.swift
+++ SwiftyNinja/GameScene.swift
@@ -9,7 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
+
+    var gameScore: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+
     override func didMoveToView(view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        background.zPosition = -1
+        addChild(background)
+
+        // CGVector represents Earth's gravity; CGVector can be visualized as a vector
+        // with base at (0,0) and points to location (x,y). here the vector points straight down.
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        // the speed is 0.85 which is slightly less than Earth's gravity of 0.98
+        physicsWorld.speed = 0.85
+
+        createScore()
+        createLives()
+        createSlices()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -17,5 +44,46 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .Left
+        gameScore.fontSize = 48
+
+        addChild(gameScore)
+
+        gameScore.position = CGPoint(x: 8, y: 8)
+    }
+
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            addChild(spriteNode)
+
+            livesImages.append(spriteNode)
+        }
+    }
+
+    // swiping around the screen will leave a glowing trail of slice marks that fade away
+    // when you let go or keep moving
+    func createSlices() {
+        // draw 2 slice shapes which are placed above everything else in the game
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+
+        // the background slice is in yellow and the foreground in white
+        // the background slice has a thicker line with than the foreground
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        activeSliceFG.strokeColor = UIColor.whiteColor()
+        activeSliceFG.lineWidth = 5
+
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
     }
 }
